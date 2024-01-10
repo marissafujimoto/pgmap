083d626ee1aa364c5499d68565664889a4e43b74
Candace Savonen <cansav09@gmail.com>
2024-01-10 14:12:55 -0500

    Add data

--- R/utils.R
+++ R/utils.R
@@ -1,18 +1,10 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
-
-hello <- function() {
-  print("Hello, world!")
+#' Get file path to an key encryption RDS
+example_data <- function() {
+  file <- list.files(
+    pattern = "PP_pgPEN_HeLa_counts.txt",
+    recursive = TRUE,
+    system.file("extdata", package = "metricminer"),
+    full.names = TRUE
+  )
+  readr::read_tsv(file)
 }
