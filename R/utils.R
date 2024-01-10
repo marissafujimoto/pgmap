f8401f2662004cc984bac927e5776b3f2c09366c
Candace Savonen <cansav09@gmail.com>
2024-01-10 15:15:38 -0500

    Update docs

--- R/utils.R
+++ R/utils.R
@@ -1,12 +1,16 @@
-#' Getting Example Data
-#' @description This function pulls some example pgRNA CRIPSR screen data with which to try the tutorials
-#' @importFrom readr read_tsv
+#' This is a title for a function
+#' @description This is a function here's where we describe what it does
+#' @param parameter Here's a parameter let's describe it here
 #' @export
+#' @examples \dontrun{
+#'
+#' pg_data <- example_data()
+#' }
 example_data <- function() {
   file <- list.files(
     pattern = "PP_pgPEN_HeLa_counts.txt",
     recursive = TRUE,
-    system.file("extdata", package = "metricminer"),
+    system.file("extdata", package = "gimap"),
     full.names = TRUE
   )
   readr::read_tsv(file)
