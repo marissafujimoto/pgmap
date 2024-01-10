fd5465e20cf23789f70e1a82b9b4849f101d44db
Candace Savonen <cansav09@gmail.com>
2024-01-10 14:47:54 -0500

    Add boilerplate function

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,7 @@
-#' Get file path to an key encryption RDS
+#' Getting Example Data
+#' @description This function pulls some example pgRNA CRIPSR screen data with which to try the tutorials
+#' @importFrom readr read_tsv
+#' @export
 example_data <- function() {
   file <- list.files(
     pattern = "PP_pgPEN_HeLa_counts.txt",
