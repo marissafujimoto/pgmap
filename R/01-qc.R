c19e42999af085b0d86d0e7c6f55261763b97152
Candace Savonen <cansav09@gmail.com>
2024-01-11 21:37:04 -0500

    More work -- getting together the datasetup structure!

--- R/01-qc.R
+++ R/01-qc.R
@@ -1,25 +1,39 @@
 
-#' Check data format
-#' @description This is a function here's where we describe what it does
-#' @param parameter Here's a parameter let's describe it here
-#' @export
-#' @examples \dontrun{
-#'
-#' }
-
-check_format <- function() {
-
-}
-
-
 #' This is a title for a function
 #' @description This is a function here's where we describe what it does
 #' @param parameter Here's a parameter let's describe it here
 #' @export
+#' @importFrom tidyr pivot_longer
+#' @import ggplot
 #' @examples \dontrun{
 #'
 #' }
 
-run_qc <- function() {
+run_qc <- function(gimap_data, plots_dir = "./qc_plots", wide_ar = 0.75, square_ar = 1) {
+
+  if (!dir.exists(plots_dir)) {
+    dir.create(plots_dir, showWarnings = TRUE)
+  }
+
+  if (!("gimap_dataset" %in% class(gimap_data))) stop("This function only works with gimap_dataset objects which can be made with the setup_data() function.")
+
+  long_form <-
+    tidyr::pivot_longer(data.frame(gimap_dataset$transformed_data$count_norm),
+                        everything(),
+                        names_to = "sample",
+                        values_to = "count normalized")
+
+  counts_cdf <- ggplot(long_form, aes(x = count, color = sample)) +
+    stat_ecdf() +
+    labs(x = "-log10(count/total_count)", # bquote(~-log[10]~"(count/total_count)")
+         y = "Expected_pgRNAs",
+         color = "Sample") +
+    plot_options() +
+    plot_theme() +
+    theme(aspect.ratio = wide_ar)
+
+  counts_cdf
+  save_plot(counts_cdf)
+
 
 }
--- R/utils.R
+++ R/utils.R
@@ -16,47 +16,52 @@ example_data <- function() {
   readr::read_tsv(file)
 }
 
-# TODO: Candace looks into what kind of R class we want
-gimap_data <- R6::R6Class("gimap_data",
-            public = list(
-              metadata = NULL,
-              counts = NULL,
-              counts_per_sample = NULL
-            ))
 
-#' Making a new gimap dataset
-#' @description This function allows people to have their data ready to be processed by gimap
-#' @param metadata
-#' @param counts
-#' @export
-#' @examples \dontrun{
-#' data <- example_data() %>%
-#'   dplyr::select(c("Day00_RepA", "Day05_RepA", "Day22_RepA", "Day22_RepB", "Day22_RepC")) %>%
-#'   as.matrix()
-#'
-#' counts_data <- setup_data(data)
-#' }
-setup_data <- function(metadata = NULL, counts = NULL) {
-
-  new_data <- gimap_data$new()
-
-  if (!is.null(counts)) stop("counts cannot be NULL")
-  if (!is.matrix(counts)) stop("counts can only be in the form of a matrix")
-
-  new_data$counts <- counts
-
-  new_data$counts_per_sample <- apply(counts, 2, sum)
-
-  if (!is.null(metadata)) {
-    if (!is.data.frame(metadata)) stop("metadata can only be in the form of a data.frame")
-
-    if (!nrow(metadata) != ncol(counts)) {
-      stop("the number of rows in the metadata is not equal to the number of columns in counts.
-           Are you sure this metadata goes with these counts?")
-    }
-    new_data$metadata <- metadata
-  }
-
-  return(new_data)
+## ggplot themes
+## see: https://www.rdocumentation.org/packages/ggplot2/versions/2.1.0/topics/theme_update
+## and https://stackoverflow.com/questions/23173915/can-ggplot-theme-formatting-be-saved-as-an-object
+plot_theme <- function() {
+  theme(axis.text = element_text(colour="black"),
+        axis.ticks = element_line(color="black"))
 }
 
+plot_options <- function() {
+  list(theme_bw(base_size = 12))
+}
+
+print_kbl <- function(tbl) {
+  kbl(tbl) %>%
+    kable_styling(full_width = FALSE,
+                  position = "left",
+                  bootstrap_options = c("striped", "hover", "responsive"))
+}
+
+save_tbl <- function(tbl){
+  tbl_str <- deparse(substitute(tbl))
+  tbl_name <- str_split(tbl_str, pattern = "\\.")[[1]][2]
+  write_tsv(tbl, file.path(out_dir, "tables", "tsv", paste0(params$cell_line, "_", tbl_name, ".txt")))
+  write_rds(tbl, file.path(out_dir, "tables", "rds", paste0("d.", params$cell_line, "_", tbl_name)))
+}
+
+
+save_plot <- function(plt){
+  plt_str <- deparse(substitute(plt))
+  ggsave(plot = plt,
+         filename = file.path(out_dir, "plots", "pdf", paste0(params$cell_line, "_", plt_str, ".pdf")))
+  ggsave(plot = plt,
+         filename = file.path(out_dir, "plots", "png", paste0(params$cell_line, "_", plt_str, ".png")))
+}
+
+make_out_dir <- function(out_dir){
+  if(dir.exists(out_dir)){
+    ## print a message with the output directory location
+    print(paste("Output directory already exists at:", out_dir, sep = " "))
+  } else{
+    ## make output dirs
+    dir.create(file.path(out_dir, "tables", "rds"), recursive = TRUE)
+    dir.create(file.path(out_dir, "tables", "tsv"), recursive = TRUE)
+    dir.create(file.path(out_dir, "plots", "png"), recursive = TRUE)
+    dir.create(file.path(out_dir, "plots", "pdf"), recursive = TRUE)
+    print(paste("Output directory created at:", out_dir, sep = " "))
+  }
+}
