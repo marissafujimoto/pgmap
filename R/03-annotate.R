b01d765f41efffcc3441133e7038880202b98de2
Candace Savonen <cansav09@gmail.com>
2024-03-27 09:08:39 -0400

    Add more notes to annotate step

--- R/03-annotate.R
+++ R/03-annotate.R
@@ -2,7 +2,8 @@
 #' @description In this function, a `gimap_dataset` is annotated as far as which genes should be used as controls.
 #' @param .data Data can be piped in with %>% or |> from function to function. But the data must still be a gimap_dataset
 #' @param gimap_dataset A special dataset structure that is setup using the `setup_data()` function.
-#' @param annotation_file A special file that contains the list that says which genes should be considered controls or not #TODO: Figure specifics of how this file should be formatted
+#' @param gene_id_type Specify what kind of gene IDs are specified in the `pg_ids`. By default will assume gene symbol.
+#' @param control_genes A list of genes that should be labeled as control genes. These will be used for log fold change calculations.
 #' @export
 #' @examples \dontrun{
 #'
@@ -31,6 +32,15 @@ gimap_annotate <- function(.data = NULL,
   }
 
   # TODO: Put the code that annotates the data here!
+
+  # This file is from https://depmap.org/portal/download/all/ and from DepMap Public 19Q3 All Files
+  # Read in from inst/extdata/Achilles_common_essentials.csv
+
+  # We'll take a look at the gimap_dataset$pg_ids and see what kinds of gene ids are there
+  # If we need to do gene conversion we'd do something like:
+
+  # biocLite('org.Hs.eg.db')
+  # mapIds(org.Hs.eg.db, <column of gene IDs>, 'ENTREZID', 'SYMBOL')
   # https://github.com/FredHutch/GI_mapping/blob/main/workflow/scripts/02-get_pgRNA_annotations.Rmd
 
   gimap_dataset$annotation <- NULL #TODO: Final step is annotations that line up to the same order as the pg gene data should be stored here.
