bbf3660532bb11caed7128e8fe7c041547622d9a
Candace Savonen <cansav09@gmail.com>
2024-04-11 11:50:32 -0400

    Adding annotation steps

--- R/03-annotate.R
+++ R/03-annotate.R
@@ -1,6 +1,8 @@
 #' Annotate gimap data
 #' @description In this function, a `gimap_dataset` is annotated as far as which genes should be used as controls.
 #' @param .data Data can be piped in with %>% or |> from function to function. But the data must still be a gimap_dataset
+#' @param cell_line which cell line are you using?
+#' @param cn_annotate TRUE or FALSE you'd also like to have Copy number annotation from DepMap
 #' @param gimap_dataset A special dataset structure that is setup using the `setup_data()` function.
 #' @param annotation_file If no file is given, will attempt to use the design file from https://media.addgene.org/cms/filer_public/a9/9a/a99a9328-324b-42ff-8ccc-30c544b899e4/pgrna_library.xlsx
 #' @param control_genes A list of genes that should be labeled as control genes. These will be used for log fold change calculations. If no list is given then DepMap Public 23Q4 Achilles_common_essentials.csv is used https://depmap.org/portal/download/all/
@@ -22,7 +24,6 @@
 gimap_annotate <- function(.data = NULL,
                            gimap_dataset,
                            annotation_file = NULL) {
-
   if (!is.null(.data)) gimap_dataset <- .data
 
   if (!("gimap_dataset" %in% class(gimap_dataset))) stop("This function only works with gimap_dataset objects which can be made with the setup_data() function.")
@@ -40,28 +41,69 @@ gimap_annotate <- function(.data = NULL,
   } else {
     # This file is from https://depmap.org/portal/download/all/ and from DepMap Public 19Q3 All Files
     # Essential gene labeling is from inst/extdata/Achilles_common_essentials.csv
-    control_genes <- get_example_data("crtl_genes")
-    control_genes <- stringr::word(control_genes$gene, sep = " \\(", 1)
+    control_genes <- readr::read_tsv("https://figshare.com/ndownloader/files/40448429", show_col_types = FALSE)
+    control_genes <- control_genes %>%
+      tidyr::separate(col = gene, into = c("gene_symbol", "entrez_id"), remove = FALSE, extra = "drop")
   }
 
-  annotation_df <-annotation_df %>%
-    dplyr::mutate(
-      gene_essential_flag = gene1_symbol %in% control_genes | gene2_symbol %in% control_genes
-      )
+  ## get TPM and CN information (w/ option for user to upload their own info)
+  depmap_metadata <- readr::read_csv("https://figshare.com/ndownloader/files/35020903", show_col_types = FALSE)
 
-  # This is the core of the code we'll need but we'll need to refactor
-  annotation_df  <- annotation_df  %>%
-  dplyr::mutate(norm_ctrl_flag = dplyr::case_when(
-    target_type == "gene_gene" ~ "double_targeting",
-    gene_essential_flag == TRUE ~ "positive_control",
-    gene_essential_flag != TRUE ~ "single_targeting",
-    target_type == "ctrl_ctrl" ~ "negative_control")) %>%
-  dplyr::mutate(norm_ctrl_flag = factor(norm_ctrl_flag, levels = c("negative_control",
-                                                            "positive_control",
-                                                            "single_targeting",
-                                                            "double_targeting")))
+  ## Get TPM data
+  my_depmap_id <- depmap_metadata %>%
+    filter(stripped_cell_line_name == cell_line) %>%
+    pull(DepMap_ID)
 
-  gimap_dataset$annotation <- NULL #TODO: Final step is annotations that line up to the same order as the pg gene data should be stored here.
+  tpm_file <- file.path(system.file("extdata", package = "gimap"), "CCLE_expression.csv")
+  if (!file.exists(tpm_file)) tpm_setup()
+
+  depmap_tpm <- readr::read_csv(tpm_file,
+    show_col_types = FALSE,
+    col_select = c("genes", my_depmap_id)
+  ) %>%
+    rename(log2_tpm = my_depmap_id) %>%
+    mutate(expressed_flag = case_when(
+      log2_tpm < 1 ~ FALSE,
+      log2_tpm >= 1 ~ TRUE,
+      is.na(log2_tpm) ~ NA
+    ))
+
+  if (cn_annotate) {
+    cn_file <- file.path(system.file("extdata", package = "gimap"), "CCLE_gene_cn.csv")
+    if (!file.exists(cn_file)) cn_setup()
+
+    depmap_cn <- readr::read_csv(cn_file,
+      show_col_types = FALSE,
+      col_select = c("genes", my_depmap_id)
+    ) %>%
+      rename(log2_cn = my_depmap_id)
+
+    annotation_df <- annotation_df %>%
+      dplyr::left_join(depmap_cn, by = c("gene1_symbol" = "genes")) %>%
+      dplyr::left_join(depmap_cn, by = c("gene2_symbol" = "genes"))
+  }
+
+  annotation_df <- annotation_df %>%
+    dplyr::left_join(depmap_tpm, by = c("gene1_symbol" = "genes")) %>%
+    dplyr::rename(gene1_essential_flag = expressed_flag) %>%
+    dplyr::left_join(depmap_tpm, by = c("gene2_symbol" = "genes")) %>%
+    dplyr::rename(gene2_essential_flag = expressed_flag) %>%
+    mutate(norm_ctrl_flag = case_when(
+      target_type == "gene_gene" ~ "double_targeting",
+      target_type == "gene_ctrl" & gene1_essential_flag == TRUE ~ "positive_control",
+      target_type == "ctrl_gene" & gene2_essential_flag == TRUE ~ "positive_control",
+      target_type == "gene_ctrl" & gene1_essential_flag != TRUE ~ "single_targeting",
+      target_type == "ctrl_gene" & gene2_essential_flag != TRUE ~ "single_targeting",
+      target_type == "ctrl_ctrl" ~ "negative_control"
+    )) %>%
+    mutate(norm_ctrl_flag = factor(norm_ctrl_flag, levels = c(
+      "negative_control",
+      "positive_control",
+      "single_targeting",
+      "double_targeting"
+    )))
+
+  gimap_dataset$annotation <- annotation_df # TODO: Final step is annotations that line up to the same order as the pg gene data should be stored here.
 
   return(gimap_dataset)
 }
