57999556b18b0333096f59838dd0984a121121f3
Kate Isaac <41767733+kweav@users.noreply.github.com>
2024-06-13 09:34:14 -0400

    Update R/plots-qc.R
    Co-authored-by: Candace Savonen <cansav09@gmail.com>

--- R/plots-qc.R
+++ R/plots-qc.R
@@ -73,7 +73,8 @@ qc_sample_hist <- function(gimap_dataset, wide_ar = 0.75) {
 #' @import ggplot2
 #' @return a ggplot histogram
 #' @examples \dontrun{
-#'
+#' gimap_dataset <- get_example_data("gimap")
+#' qc_variance_hist(gimap_dataset)
 #' }
 #'
 
