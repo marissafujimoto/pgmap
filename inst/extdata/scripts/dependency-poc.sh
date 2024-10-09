00a29ac68edf261cee0a1d7efb2d7b5c207208c5
= <=>
2024-10-09 13:06:36 -0700

    Call dependencies in bash script

--- inst/extdata/scripts/dependency-poc.sh
+++ inst/extdata/scripts/dependency-poc.sh
@@ -1,1 +1,9 @@
-echo "hello world"
+# TODO move source to /usr/src and build binaries to /usr/local/bin?
+
+/home/seqtk/seqtk-1.4/seqtk
+
+/home/config/idemp/idemp
+
+/home/biodocker/bin/bowtie2
+
+/usr/src/samtools-1.9/samtools
