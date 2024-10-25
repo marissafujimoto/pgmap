6da5ad1ebf56eedf123f55ecc93bbad13b8c08d1
Candace Savonen <cansav09@gmail.com>
2024-10-25 10:55:49 -0400

    Grab stats function

--- R/get_stats.R
+++ R/get_stats.R
@@ -1,7 +1,9 @@
-#' Obtain the counts for a group of samples
-#' @description This function takes a forward and reverse bam files for a group of samples and returns the counts and stats.
+#' Obtain the stats for a group of samples
+#' @description This function takes a forward and reverse bam files for a group of samples and returns alignment stats.
 #' @param bam_dir a file path to a directory where the bam files associated with the sample names will be stored.
 #' @param sample_name a character vector that indicates the name of the samples as they are listed in the file names.
+#' @param output_dir a file path to where you'd like the stats to be saved to. By default its "stats" folder. This folder will be 
+#' created if it doesn't exist. 
 #' @param time TRUE/FALSE you want the duration this takes to run printed out
 #' @importFrom furrr future_pmap future_map
 #' @importFrom purrr reduce
@@ -17,17 +19,15 @@
 #' # There needs to be exactly 2 bam files per sample
 #' sample_names <- c("sample1", "sample2", "sample3")
 #'
-#' counts_df <- calc_counts(
+#' stats_df <- get_stats(
 #'   bam_dir = bam_dir,
 #'   sample_names = sample_names,
-#'   time = TRUE
+#'   output_dir = "output_dir"
 #' )
 #'
 #' # We can write these to CSV files like this
-#' readr::write_csv(counts_df$counts, "counts_pgmap.tsv")
-#' readr::write_csv(counts_df$stats, "stats_pgmap.tsv")
 #' }
-get_stats <- function(bam_dir, sample_names, time = TRUE) {
+get_stats <- function(bam_dir, sample_names, output_dir = "stats", time = FALSE) {
   if (time) timing <- Sys.time()
 
   # Grab the file names
@@ -36,57 +36,21 @@ get_stats <- function(bam_dir, sample_names, time = TRUE) {
     sample_names = sample_names
   )
 
+  # Make output directory if it doesn't exist
+  dir.create(output_dir, showWarnings = TRUE, recursive = TRUE)
+  
   # Now use the custom function to get the counts for each sample from the pair of bam files
   stats <- furrr::future_pmap(sample_df, function(sample_name, forward_file, reverse_file) {
-    # Read in the BAM files
-    bam_1_stats <- quickBamFlagSummary(forward_file)
-    bam_2_stats <- quickBamFlagSummary(reverse_file)
+    # Get stats and save to files
+    sink(file.path(output_dir, paste0(sample_name, "_F_stats.txt")), type = "output")
+    quickBamFlagSummary(forward_file)
+    sink()
+    sink(file.path(output_dir, paste0(sample_name, "_F_stats.txt")), type = "output")
+    quickBamFlagSummary(forward_file)
+    sink()
   })
-  # Bring along sample names
-  names(counts) <- sample_names
-
-  # Then condense all samples to one stats data frame
-  stats_df <- furrr::future_map(counts, "stats") %>%
-    dplyr::bind_rows(.id = "sample")
-
-  # Then condense all samples to one counts data frame
-  counts_df <- furrr::future_map(counts, "counts", right_join) %>%
-    purrr::reduce(dplyr::full_join)
 
   if (time) message(paste0("Done(", signif(as.numeric(difftime(Sys.time(), timing, units = "mins")), 2), "m elapsed).\n"))
   # Return as a list of two data frames
-  return(list(counts = counts_df, stats = stats_df))
-}
-
-#' Obtain the counts for a single sample
-#' @description This function takes a forward and reverse bam file and returns the counts
-#' @param bam_1 a file path to one of the bam files for the sample
-#' @param bam_2 a file path to the other one of the bam files for the sample
-#' @param sample_name a single character that indicates the name of the sample "sample1"
-#' @param time TRUE/FALSE you want the duration this takes to run printed out
-#' @import dplyr
-#' @importFrom Rsamtools ScanBamParam scanBam
-#' @export
-#' @examples \dontrun{
-#'
-#' bam_dir <- file.path(example_data_folder(), "bam")
-#'
-#' counts <- sample_stats(
-#'   bam_1 = file.path(bam_dir, "pgMAP_tutorial_gRNA1_trimmed_sample1_aligned.bam"),
-#'   bam_2 = file.path(bam_dir, "pgMAP_tutorial_gRNA2_trimmed_sample1_aligned.bam"),
-#'   sample_name = "sample1",
-#'   time = TRUE
-#' )
-#' }
-sample_stats <- function(bam_1, bam_2, sample_name, time = FALSE) {
-  if (time) timing <- Sys.time()
-  # Print out message
-  message(paste("Getting stats for", sample_name))
-
-  # Read in the BAM files
-  bam_1 <- quickBamFlagSummary(bam_1, param = param)
-  bam_2 <- quickBamFlagSummary(bam_2, param = param)
-
-  if (time) message(paste0("Done(", signif(as.numeric(difftime(Sys.time(), timing, units = "mins")), 2), "m elapsed).\n"))
-  return(list(counts = counts_df, stats = stats_df))
+  message("Stats saved to", output_dir)
 }
