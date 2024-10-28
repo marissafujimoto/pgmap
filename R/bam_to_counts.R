6c0b8872d3b0c1d9143441fc364319c4547c274e
Candace Savonen <cansav09@gmail.com>
2024-10-28 10:17:56 -0400

    updates

--- R/bam_to_counts.R
+++ R/bam_to_counts.R
@@ -1,7 +1,7 @@
 #' Obtain the counts for a group of samples
 #' @description This function takes a forward and reverse bam files for a group of samples and returns the counts and stats.
 #' @param bam_dir a file path to a directory where the bam files associated with the sample names will be stored. 
-#' @param sample_name a character vector that indicates the name of the samples as they are listed in the file names. 
+#' @param sample_names a character vector that indicates the name of the samples as they are listed in the file names. 
 #' @param time TRUE/FALSE you want the duration this takes to run printed out
 #' @importFrom furrr future_pmap future_map 
 #' @importFrom purrr reduce
