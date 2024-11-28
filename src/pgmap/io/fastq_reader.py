db0927538017b0aeee434c5ea86c7abc84f2f608
marissafujimoto <fujim@uw.edu>
2024-11-27 18:42:58 -0800

    Add fastq reading function

--- src/pgmap/io/fastq_reader.py
+++ src/pgmap/io/fastq_reader.py
@@ -1,2 +1,32 @@
-def hello_world() -> str:
-    return "Hello World"
+from Bio import SeqIO
+from mimetypes import guess_type
+
+import gzip
+from typing import IO, Iterable
+
+
+def read_fastq(fastq_path: str) -> Iterable[str]:
+    """
+    Read sequences from a fastq (ignoring quality)
+
+    Args:
+        fastq_path (str): The path to a fastq file. Can optionally be gzipped
+
+    Yields:
+        str: The next sequence in the fastq file
+    """
+    # TODO check file validity?
+
+    encoding = guess_type(fastq_path)[1]
+
+    if encoding == "gzip":
+        with gzip.open(fastq_path, "rt") as fastq_file:
+            yield from _generate_sequences(fastq_file)
+    else:
+        with open(fastq_path) as fastq_file:
+            yield from _generate_sequences(fastq_file)
+
+
+def _generate_sequences(fastq_file: IO) -> Iterable[str]:
+    for read in SeqIO.parse(fastq_file, "fastq"):
+        yield read.seq
--- tests/__main__.py
+++ tests/__main__.py
@@ -2,10 +2,26 @@ import unittest
 
 from pgmap.io import fastq_reader
 
+
 class TestIO(unittest.TestCase):
 
-    def test_hello_world(self):
-        self.assertEqual(fastq_reader.hello_world(), "Hello World")
+    def test_read_fastq(self):
+        count = 0
+
+        # TODO move these files to a cleaner test data folder
+        for sequence in fastq_reader.read_fastq("inst/extdata/data/fastqs/fastq_trimmed/pgMAP_tutorial_gRNA1_trimmed.fastq"):
+            count += 1
+
+        self.assertEqual(count, 10e5)
+
+    def test_read_fastq_gz(self):
+        count = 0
+
+        for sequence in fastq_reader.read_fastq("inst/extdata/data/fastqs/PP_pgPEN_HeLa_S1_R3_001.fastq.gz"):
+            count += 1
+
+        self.assertEqual(count, 10e5)
+
 
 if __name__ == "__main__":
     unittest.main()
