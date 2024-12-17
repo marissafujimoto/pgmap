3d72cb7041d16a9cb35750754f9f6253605de179
marissafujimoto <fujim@uw.edu>
2024-12-17 14:57:13 -0800

    Rename trim strategy attributes

--- src/pgmap/trimming/read_trimmer.py
+++ src/pgmap/trimming/read_trimmer.py
@@ -18,12 +18,12 @@ def two_read_trim(R1_path: str, I1_path: str, ) -> Iterable[PairedRead]:
         candidates (PairedRead): Trimmed paired reads.
     """
     # TODO include some kind of citation or link to the two read sequencing strategy?
-    R1_trim_coord = TrimCoordinate(file_index=0, start=0, end=20)
-    R2_trim_coord = TrimCoordinate(file_index=1, start=1, end=21)
+    gRNA1_trim_coord = TrimCoordinate(file_index=0, start=0, end=20)
+    gRNA2_trim_coord = TrimCoordinate(file_index=1, start=1, end=21)
     barcode_trim_coord = TrimCoordinate(file_index=1, start=160, end=166)
 
     trim_strategy = TrimStrategy(
-        R1=R1_trim_coord, R2=R2_trim_coord, barcode=barcode_trim_coord)
+        gRNA1=gRNA1_trim_coord, gRNA2=gRNA2_trim_coord, barcode=barcode_trim_coord)
 
     yield from trim([R1_path, I1_path], trim_strategy)
 
@@ -40,12 +40,12 @@ def three_read_trim(R1_path: str, R2_path: str, I1_path: str) -> Iterable[Paired
         candidates (PairedRead): Trimmed paired reads.
     """
     # TODO include some kind of citation or link to the three read sequencing strategy?
-    R1_trim_coord = TrimCoordinate(file_index=0, start=0, end=20)
-    R2_trim_coord = TrimCoordinate(file_index=1, start=1, end=21)
+    gRNA1_trim_coord = TrimCoordinate(file_index=0, start=0, end=20)
+    gRNA2_trim_coord = TrimCoordinate(file_index=1, start=1, end=21)
     barcode_trim_coord = TrimCoordinate(file_index=2, start=0, end=6)
 
     trim_strategy = TrimStrategy(
-        R1=R1_trim_coord, R2=R2_trim_coord, barcode=barcode_trim_coord)
+        gRNA1=gRNA1_trim_coord, gRNA2=gRNA2_trim_coord, barcode=barcode_trim_coord)
 
     yield from trim([R1_path, R2_path, I1_path], trim_strategy)
 
@@ -62,8 +62,8 @@ def trim(fastq_paths: list[str], trim_strategy: TrimStrategy) -> Iterable[Paired
         candidates (PairedRead): Trimmed paired reads.
     """
     for raw in zip(*list(map(read_fastq, fastq_paths))):
-        candidate = PairedRead(gRNA1_candidate=raw[trim_strategy.R1.file_index][trim_strategy.R1.start:trim_strategy.R1.end],
-                               gRNA2_candidate=raw[trim_strategy.R2.file_index][trim_strategy.R2.start:trim_strategy.R2.end],
+        candidate = PairedRead(gRNA1_candidate=raw[trim_strategy.gRNA1.file_index][trim_strategy.gRNA1.start:trim_strategy.gRNA1.end],
+                               gRNA2_candidate=raw[trim_strategy.gRNA2.file_index][trim_strategy.gRNA2.start:trim_strategy.gRNA2.end],
                                barcode_candidate=raw[trim_strategy.barcode.file_index][trim_strategy.barcode.start:trim_strategy.barcode.end])
 
         yield candidate
