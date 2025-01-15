383366e660537a41b1c064a4a292e14b71939134
marissafujimoto <fujim@uw.edu>
2025-01-14 16:07:13 -0800

    Add gRNA cached aligner docs

--- src/pgmap/alignment/grna_cached_aligner.py
+++ src/pgmap/alignment/grna_cached_aligner.py
@@ -1,8 +1,24 @@
 from typing import Iterable
 
 
-def construct_grna_error_alignment_cache(gRNAs: list[str], gRNA_error_tolerance: int) -> dict[str, tuple[str, int]]:
-    # TODO docs
+def construct_grna_error_alignment_cache(gRNAs: Iterable[str], gRNA_error_tolerance: int) -> dict[str, tuple[str, int]]:
+    """
+    Construct an alignment cache object containing all gRNAs with the error tolerance amount of substitutions. The 
+    number of gRNAs within the error tolerance grows exponentially. As such this function should only be used for
+    error tolerances of 0 or 1.
+
+    Args:
+        gRNAs (Iterable[str]): An iterable producing all the gRNAs to construct the alignment cache from.
+        gRNA_error_tolerance (int): The error tolerance used to create gRNA alignment candidates.
+
+    Returns:
+        alignment_cache (dict[str, tuple[str, int]]): A mapping from each valid alignment string to a tuple of the
+        reference alignment sequence and the hamming distance from the reference. Guarantees a minimum alignment,
+        though there could be multiple best alignments depending on the gRNAs.
+
+    Raises:
+        ValueError: if gRNA_error_tolerance is not 0 or 1.
+    """
 
     if gRNA_error_tolerance > 1 or gRNA_error_tolerance < 0:
         raise ValueError(f"gRNA error tolerance must be 0 or 1 but was {
